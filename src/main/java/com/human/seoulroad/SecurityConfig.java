18a80428cd64ad289d4669568428889d3ac813e5
황지훈 <rkgh17@gmail.com>
2023-01-19 15:08:43 +0900

    hjh_119

--- src/main/java/com/human/seoulroad/SecurityConfig.java
+++ src/main/java/com/human/seoulroad/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig{
 	
 	@Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf().disable();
         http.authorizeHttpRequests().requestMatchers(
                 new AntPathRequestMatcher("/**")).permitAll()
         
